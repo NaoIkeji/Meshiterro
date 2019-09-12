377ec6371148c2e5379322ce38f7950f18690f9d
NaoIkeji <na3o.g.7@gmail.com>
2019-09-12 03:21:16 +0000

    [Finish]5-3_ログイン画面/画像投稿機能、投稿画面、詳細ページ/トップページの作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '71940643fe67dca12b596c815d17c710c144a429235b0cd5b7386c49595380ffd27b1f76f3c9aec18643468247e1a11d028126fcf898a463bfb1c5a1102d6e2c'
